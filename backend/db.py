8689a7dd0eb6a1efa287c4ca8d7704762a742c66
durgaprasadp1803 <durgaprasadp552@gmail.com>
2026-02-24 19:21:49 +0530

    docker deployment

--- backend/db.py
+++ backend/db.py
@@ -13,7 +13,7 @@ import os
 
 def get_db_connection():
     return mysql.connector.connect(
-        host=os.getenv("DB_HOST", "host.docker.internal"),
+        host=os.getenv("DB_HOST"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
         database=os.getenv("DB_NAME"),
